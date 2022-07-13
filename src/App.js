66ac825b64ad46a1af54efbfd399736a1b282c01
kimjonghyo <qaws2921@hanmail.net>
2022-07-13 16:21:01 +0900

    메인 수정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./asset/css/style.css";
 import "./asset/css/notosanskr.css";
-import "./asset/css/responsive.css";
+
 import "./asset/css/slick.css";
 import "./asset/css/swiper.min.css";
 import "./asset/css/TitilliumWeb.css";
+import "./asset/css/responsive.css";
 
 function App() {
   return (
