bfe1c8d92aaa663f04910c47fa69759b080fe68f
star-think <qaws2921@hanmail.net>
2022-07-19 21:31:06 +0900

    마무리 작업 1차

--- src/pages/company/History.js
+++ src/pages/company/History.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CompanyTop from "../../components/company/CompanyTop";
 import HisContent from "../../components/company/history/HisContent";
 import MainFooter from "../../components/MainFooter";
 import MainHeader from "../../components/MainHeader";
 
 const History = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `회사연혁 | (주)별생각`;
+  }, []);
   return (
     <>
       <MainHeader headerFix={true} />
